7028c84e43524d8d966ea53602eef1b76327b852
Chris Salter <salter_chris@hotmail.com>
2016-05-08 17:18:02 +1000

    Added card constructor and card generator
    Added constructor for Card class.
    
    Added method to draw a random card. This works by generating a random
    value in range 0-13 and using this generated value to generate the name
    and value of the card.

--- src/Card.java
+++ src/Card.java
@@ -8,12 +8,101 @@
  *
  */
 
+import java.util.Random;
+
 public class Card {
+	private String name;
+	private int value;
 	
+	//Constructor for a card
 	public Card()
 	{
-		//Constructor for a card
+		//Initialise card name and value to known safe values
+		this.name = null;
+		this.value = 0;		
+	}
+	
+	//Getter for name value
+	public String getName() {
+		return name;
+	}
+
+	//Setter for name value
+	private void setName(int value) {
 		
-		//Generate random card 
+		//Array containing all possible card names
+		String[] card_names = 
+		{
+			"Ace",
+			"One",
+			"Two",
+			"Three",
+			"Four",
+			"Five",
+			"Six",
+			"Seven",
+			"Eight",
+			"Nine",
+			"Ten",
+			"Jack",
+			"Queen",
+			"King"
+		};
+				
+		//Set card name based on value passed in
+		this.name = card_names[value];
+	}
+
+	//Setter for card value
+	private void setValue(int value) {
+		
+		//If card drawn is a Jack, Queen, or king, set value to 10.
+		if(value > 10)
+		{
+			value = 10;
+		}
+		
+		//Set value to randomly generated card's value. Ace is left as 0 as an 
+		//identifier
+		this.value = value;
+	}
+
+	//Getter for card value
+	public int getValue() {
+		return value;
+	}
+
+	//Draw a card. Generates a random number and assigns name and value based on
+	//generated value
+	public Card draw_card()
+	{
+		//Create blank card object
+		Card generated_card = new Card();
+		
+		//Generate a random number to use to set card name and value
+		int value = generate_random_value();
+		
+		//Set card name based on value generated
+		generated_card.setName(value);
+		
+		//Set card value based on value generated
+		generated_card.setValue(value);		
+		
+		//Return randomly generated card
+		return generated_card;
+	}
+	
+	//Method to generate a random number in range 0-12. This value is used to 
+	//determine card information
+	public int generate_random_value()
+	{
+		//Create random number generator
+		Random randomGenerator = new Random();
+		
+		//Generate random int in range 0-13
+		int randomInt = randomGenerator.nextInt(14);
+		
+		//Return generated int
+		return randomInt;
 	}
 }
